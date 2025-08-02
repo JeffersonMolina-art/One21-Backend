ca0b3cbf48a29e5197ba434de327118a3bb5098b
kborge <kevinborgetobar1998@gmail.com>
2025-08-02 09:36:46 -0600

    - Feat: Configuración del test inicial de carga

--- apps/notification-service/src/test/java/gt/edu/umg/notification/service/NotificationServiceApplicationTests.java
+++ apps/notification-service/src/test/java/gt/edu/umg/notification/service/NotificationServiceApplicationTests.java
@@ -1,13 +1,16 @@
 package gt.edu.umg.notification.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@Slf4j
 @SpringBootTest
 class NotificationServiceApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("Context loads test executed successfully.");
+
     }
 
 }

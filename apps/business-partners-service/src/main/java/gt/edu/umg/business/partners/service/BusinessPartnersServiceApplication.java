41aebe241ac0ea682da6ec90d9dd88374007fe49
kborge <kevinborgetobar1998@gmail.com>
2025-08-12 21:54:02 -0600

    - Feat: Configuración inicial del service business partners

---
+++ apps/business-partners-service/src/main/java/gt/edu/umg/business/partners/service/BusinessPartnersServiceApplication.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.business.partners.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessPartnersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessPartnersServiceApplication.class, args);
+    }
+
+}
---
+++ apps/business-partners-service/src/test/java/gt/edu/umg/business/partners/service/BusinessPartnersServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package gt.edu.umg.business.partners.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@Slf4j
+@SpringBootTest
+class BusinessPartnersServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        log.info("Context loads for business partners test executed successfully.");
+
+    }
+
+}

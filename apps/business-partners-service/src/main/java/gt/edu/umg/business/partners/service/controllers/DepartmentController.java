fea14bd553bfba6e75b6e2550090db8ef18e5744
kborge <kevinborgetobar1998@gmail.com>
2025-09-29 17:59:51 -0600

    Fix - Busqueda de municipios por deparamento y deparamentos por pais

--- apps/business-partners-service/src/main/java/gt/edu/umg/business/partners/service/controllers/DepartmentController.java
+++ apps/business-partners-service/src/main/java/gt/edu/umg/business/partners/service/controllers/DepartmentController.java
@@ -5,8 +5,12 @@ import gt.edu.umg.business.partners.service.entities.Department;
 import gt.edu.umg.business.partners.service.services.DepartmentService;
 import gt.edu.umg.generic.data.controllers.GenericController;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -15,5 +19,11 @@ public class DepartmentController extends GenericController<DepartmentDto, Depar
 
     public DepartmentController(DepartmentService departmentService) {
         super(departmentService);
+    }
+
+    @GetMapping("/by-country")
+    public List<DepartmentDto> findByDepartmentForCountry(@RequestParam Integer id) {
+        List<DepartmentDto> departmentDtos = findByDepartmentForCountry(id);
+        return departmentDtos;
     }
 }
--- apps/business-partners-service/src/main/java/gt/edu/umg/business/partners/service/services/DepartmentService.java
+++ apps/business-partners-service/src/main/java/gt/edu/umg/business/partners/service/services/DepartmentService.java
@@ -7,15 +7,21 @@ import gt.edu.umg.generic.data.services.GenericServiceImpl;
 import gt.edu.umg.generic.data.utils.GenericMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DepartmentService extends GenericServiceImpl<DepartmentDto, Department, Integer> {
-    private final DepartmentRepository stateRepository;
+    private final DepartmentRepository departmentRepository;
     private final GenericMapper mapper;
 
     public DepartmentService(DepartmentRepository stateRepository, GenericMapper mapper) {
         super(stateRepository, mapper, DepartmentDto.class, Department.class);
-        this.stateRepository = stateRepository;
+        this.departmentRepository = stateRepository;
         this.mapper = mapper;
     }
 
+    public List<DepartmentDto> findByDepartmentForCountry(Integer id) {
+        List<Department> departments = departmentRepository.findDepartmentByCountry_Id(id);
+        return mapper.toDtoList(departments , DepartmentDto.class);
+    }
 }

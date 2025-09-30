f45f78d68e6771c3252d1ea0eb2f5dc937f17a6c
kborge <kevinborgetobar1998@gmail.com>
2025-09-29 21:04:21 -0600

    Feat - Busqueda por partner id

--- apps/business-partners-service/src/main/java/gt/edu/umg/business/partners/service/repositories/AddressRepository.java
+++ apps/business-partners-service/src/main/java/gt/edu/umg/business/partners/service/repositories/AddressRepository.java
@@ -4,6 +4,10 @@ import gt.edu.umg.business.partners.service.entities.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Integer> {
+
+    public List<Address> findByBusinessPartner_Id(Integer id);
 }

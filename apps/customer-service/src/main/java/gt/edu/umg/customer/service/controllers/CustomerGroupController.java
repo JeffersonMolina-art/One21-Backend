2eeb0f8ca166e4ca765fd853a054691cea01e0d1
Sincal200 <sincaljulio200@gmail.com>
2025-10-04 10:59:16 -0600

    Feat: Refactor controller request mappings to remove 'employees' prefix

--- apps/customer-service/src/main/java/gt/edu/umg/customer/service/controllers/CustomerGroupController.java
+++ apps/customer-service/src/main/java/gt/edu/umg/customer/service/controllers/CustomerGroupController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("customers/groups")
+@RequestMapping("groups")
 public class CustomerGroupController extends GenericController<CustomerGroupDto, CustomerGroup, Integer> {
 
     public CustomerGroupController(CustomerGroupService customerGroupService) {super(customerGroupService);}
